d575b312c6dbf49cc3b87bce477d35c77c5d4ea5
Brayan <brayan.goncalves@squadts.com.br>
2023-03-28 10:31:28 -0300

    things gonna change

--- src/utils/database/database.js
+++ src/utils/database/database.js
@@ -4,29 +4,29 @@ const uri = process.env.DATABASE_URI;
 
 
 
+// function ConnectDB() {
+//   mongoose
+//     .connect(uri)
+//     .then(() => {
+//       console.log('MongoDB conectado!');
+//     })
+//     .catch((err) => {
+//       return console.log(`Deu ruim meu bom ${err}`);
+//     });
+// }
+
 function ConnectDB() {
   mongoose
-  .connect(uri)
+    .connect(uri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    })
     .then(() => {
-      console.log('MongoDB conectado!');
+      console.log("MongoDB conectado!")
     })
     .catch((err) => {
-      return console.log(`Deu ruim meu bom ${err}`);
-    });
+      return console.log(`Deu ruim meu bom ${err}`)
+    })
 }
-
-// function  {
-//   mongoose
-//   .connect(uri, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   })
-//   .then(()=> {
-//       console.log("MongoDB conectado!")
-//   })
-//   .catch((err) => {
-//       return console.log(`Deu ruim meu bom ${err}`)
-//   })
-// }
 
 module.exports = ConnectDB;
---
+++ src/utils/models/user.model.js
@@ -0,0 +1,12 @@
+// const mongoose = require('mongoose')
+
+// const UserSchema = new mongoose.Schema({
+//     name: { type: String, required: true, trim: true },
+//     email: { type: String, required: true, trim: true },
+//     password: { type: String, required: true, trim: true },
+//     photo: { type: Buffer }
+// })
+
+// const user = mongoose.model('users', UserSchema)
+
+// module.exports = user
